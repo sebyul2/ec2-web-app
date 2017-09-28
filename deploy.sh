3ef5f42fe1f0fecaa49e38fb67975a9a6c1b31fd
DESKTOP-ESEAPOT\Byul <sebyul@technoblood.com>
2017-09-28 12:28:20 +0900

    Fix bug fix deploy

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ DOCKER_APP_NAME=webapp
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml pull
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml build
     docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
 
     sleep 10
 
     docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml pull
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml build
     docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
 
     sleep 10

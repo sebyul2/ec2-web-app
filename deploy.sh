3fbabb708e9e6678ff5b6ec31bd27f04a83cf555
DESKTOP-ESEAPOT\Byul <sebyul@technoblood.com>
2017-09-28 12:51:17 +0900

    Fix add git pull

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 DOCKER_APP_NAME=webapp
-
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
+git pull origin develop
 
 if [ -z "$EXIST_BLUE" ]; then
     docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml build

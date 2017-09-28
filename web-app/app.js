26b920c99ea563fe75a0592037673ab969e591e2
DESKTOP-ESEAPOT\Byul <sebyul@technoblood.com>
2017-09-28 11:22:23 +0900

    Fix docker-compose environment 추가

--- web-app/app.js
+++ web-app/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.port || 4000;
 
 app.get('/', function (req, res) {
     res.send('Hello Fucking World!');

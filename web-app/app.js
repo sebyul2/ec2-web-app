a1193c468876bea2ba559ab8cebc31b91c916fc2
DESKTOP-ESEAPOT\Byul <sebyul@technoblood.com>
2017-09-28 11:09:39 +0900

    Fix app Port

--- web-app/app.js
+++ web-app/app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.port || 3000;
 
 app.get('/', function (req, res) {
     res.send('Hello Fucking World!');
 });
 
-app.listen(4000, function () {
-    console.log('Example app listening on port 4000!');
+app.listen(port, function () {
+    console.log('Example app listening on port ', port, '!');
 });

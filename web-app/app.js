c4997f7ed19ec72e338496aa841c8bd89cc40340
DESKTOP-ESEAPOT\Byul <sebyul@technoblood.com>
2017-09-28 11:54:37 +0900

    Fix env 수정

--- web-app/app.js
+++ web-app/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 4000;
+var port = process.env.port;
 
 app.get('/', function (req, res) {
     res.send('Hello Fucking World!');
